257c004ce09ce6b86fb8f6bb72fac102c5d92d61
nachiketbhuta <nachiketbhuta@gmail.com>
2020-03-30 15:49:48 +0530

    Removed README

--- main.lua
+++ main.lua
@@ -1,21 +1,3 @@
--- function love.load() --资源加载回调函数，仅初始化时调用一次
-
--- end
-
--- function love.draw() --绘图回调函数，每周期调用
-
--- end
-
--- function love.update(dt) --更新回调函数，每周期调用
-
--- end
-
--- function love.keypressed(key) --键盘检测回调函数，当键盘事件触发是调用
-
--- end
-
-
-
 function love.load()
 
 	passsound = love.audio.newSource("sounds/pass.ogg", "static");love.audio.stop(passsound)
